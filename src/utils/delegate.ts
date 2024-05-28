9df5a9352afcea2cadb1c0c8f7bd010c496919b3
gibidon <malykhinig@gmail.com>
2024-05-28 10:03:48 +0700

    chore: added eslint and prettier

--- src/utils/delegate.ts
+++ src/utils/delegate.ts
@@ -2,11 +2,11 @@ export function delegate(
 	box: HTMLElement,
 	selector: string,
 	eventName: string,
-	handler: Function
+	handler: (e: Event) => void
 ) {
 	box.addEventListener(eventName, function (e: Event) {
 		if (e.target instanceof HTMLElement) {
-			let elem = e.target.closest(selector)
+			const elem = e.target.closest(selector)
 			if (elem !== null && box.contains(elem)) {
 				handler.call(elem, e)
 			}
