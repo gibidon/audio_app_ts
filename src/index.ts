64d3054048f1e49b0bc33abf0fb5b7f6ef9fa8c1
gibidon <malykhinig@gmail.com>
2024-06-02 23:44:09 +0700

    chore: same cards using classes

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
-import { ICard } from './types';
-import './index.scss';
+import { ICard } from './types'
+import './index.scss'
 
-import rainBg from './assets/rainy-bg.jpg';
-import winterBg from './assets/winter-bg.jpg';
-import summerBg from './assets/summer-bg.jpg';
+import rainBg from './assets/rainy-bg.jpg'
+import winterBg from './assets/winter-bg.jpg'
+import summerBg from './assets/summer-bg.jpg'
 
-import rainAudio from './assets/sounds/rain.mp3';
-import winterAudio from './assets/sounds/winter.mp3';
-import summerAudio from './assets/sounds/summer.mp3';
+import rainAudio from './assets/sounds/rain.mp3'
+import winterAudio from './assets/sounds/winter.mp3'
+import summerAudio from './assets/sounds/summer.mp3'
 
-import rainIcon from './assets/icons/cloud-rain.svg';
-import winterIcon from './assets/icons/cloud-snow.svg';
-import summerIcon from './assets/icons/sun.svg';
-import pauseIcon from './assets/icons/pause.svg';
+import rainIcon from './assets/icons/cloud-rain.svg'
+import winterIcon from './assets/icons/cloud-snow.svg'
+import summerIcon from './assets/icons/sun.svg'
+import pauseIcon from './assets/icons/pause.svg'
 
-import { delegate } from './utils/delegate';
-
-const body = document.querySelector('body');
-const cardWrapper = body.querySelector('.card_wrapper') as HTMLElement;
-const volumeControl = document.querySelector('.volume_control') as HTMLInputElement;
+const body = document.querySelector('body')
+const cardWrapper = body.querySelector('.card_wrapper') as HTMLElement
+const volumeControl = document.querySelector('.volume_control') as HTMLInputElement
 
 const weather_cards: ICard[] = [
   {
@@ -39,66 +37,66 @@ const weather_cards: ICard[] = [
     audioUrl: summerAudio,
     iconUrl: summerIcon,
   },
-];
+]
 
-function createCard({ id, audioUrl, imageUrl, iconUrl }: ICard): HTMLDivElement {
-  const cardItem = document.createElement('div');
+class WeatherCard {
+  private rootElem: HTMLElement
+  public audio: HTMLAudioElement
+  public iconElem: HTMLImageElement
+  public icon: string
 
-  cardItem.dataset.id = `${id}`;
-  cardItem.classList.add('card');
+  constructor(selector: string, audioUrl: string, icon: string) {
+    this.rootElem = document.querySelector(selector)
+    this.audio = this.rootElem.querySelector('audio')
+    this.iconElem = this.rootElem.querySelector('.icon')
 
-  const cardImage = document.createElement('img');
-  cardImage.src = imageUrl;
-  const cardAudio = document.createElement('audio');
-  cardAudio.src = audioUrl;
-  const cardIcon = document.createElement('img');
-  cardIcon.src = iconUrl;
-  cardIcon.classList.add('card_icon');
+    this.audio.src = audioUrl
+    this.icon = icon
 
-  cardItem.appendChild(cardImage);
-  cardItem.appendChild(cardAudio);
-  cardItem.appendChild(cardIcon);
-
-  return cardItem;
-}
-
-const cardItems = weather_cards.map((card) => createCard(card));
-
-cardItems.forEach((cardItem) => cardWrapper.appendChild(cardItem));
-
-const allAudios = Array.from(document.querySelectorAll('audio'));
-
-function pauseAllAudios(): void {
-  allAudios.forEach((audio) => audio.pause());
-}
-
-delegate(cardWrapper, '.card', 'click', function (e: MouseEvent) {
-  const eventTarget = e.target as HTMLElement;
-  const clickedCardItem = eventTarget.closest('.card') as HTMLElement;
-
-  const cardData = weather_cards.find(
-    (card) => card.id === Number(clickedCardItem.dataset.id),
-  );
-
-  const clickedCardIcon = clickedCardItem.querySelector('.card_icon') as HTMLImageElement;
-
-  const clickedCardAudio = clickedCardItem.querySelector('audio') as HTMLAudioElement;
-
-  if (clickedCardAudio.paused) {
-    pauseAllAudios();
-    clickedCardAudio.play();
-    clickedCardIcon.src = cardData.iconUrl;
-  } else {
-    clickedCardAudio.pause();
-    clickedCardIcon.src = pauseIcon;
+    this.rootElem.addEventListener('click', () => this.handleClick())
+  }
+  play() {
+    this.audio.play()
+  }
+  pause() {
+    this.audio.pause()
   }
 
-  body.style.backgroundImage = `url(${cardData.imageUrl})`;
-});
+  handleClick() {
+    if (this.audio.paused) {
+      this.play()
+      this.iconElem.src = this.icon
+      stopOtherAudios(this.audio)
+    } else {
+      this.pause()
+      this.iconElem.src = pauseIcon
+    }
+  }
+}
+
+const rainCard = new WeatherCard('.rain_card', rainAudio, rainIcon)
+const winterCard = new WeatherCard('.winter_card', winterAudio, winterIcon)
+const summerCard = new WeatherCard('.summer_card', summerAudio, summerIcon)
+
+const allCards = [rainCard, winterCard, summerCard]
+
+const allAudios = Array.from(document.querySelectorAll('audio'))
+
+function stopOtherAudios(playingAudio: HTMLAudioElement) {
+  const cardsToPause = allCards.filter((card) => card.audio !== playingAudio)
+
+  cardsToPause.forEach((card) => {
+    console.log('card', card)
+    card.audio.pause()
+
+    //for correct icon showing. If audio has not been launched before,icon doesn't change to paused
+    if (card.audio.currentTime !== 0.0) {
+      card.iconElem.src = pauseIcon
+    }
+  })
+}
 
 volumeControl.addEventListener('input', () => {
-  const playingAudio = allAudios.find(
-    (audio: HTMLAudioElement) => audio.paused === false,
-  );
-  playingAudio.volume = Number(volumeControl.value);
-});
+  const playingAudio = allAudios.find((audio: HTMLAudioElement) => audio.paused === false)
+  playingAudio.volume = Number(volumeControl.value)
+})
